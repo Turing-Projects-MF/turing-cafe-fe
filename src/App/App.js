7aab35e54071ca7b061af2f0db2b38ba1cebd298
mdflynn <mdflynn34@outlook.com>
2020-12-16 09:36:47 -0700

    Create general form and display on page

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ReservationCard from '../ReservationCard/ReservationCard'
+import Form from '../Form/Form'
 
 class App extends Component {
   constructor() {
@@ -29,7 +30,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
           { this.createReservationCards() }
---
+++ src/Form/Form.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import './Form.css'
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    }
+  }
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={ this.state.name }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="text"
+          placeholder="Date (mm/dd)"
+          name="date"
+          value={ this.state.date }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="text"
+          placeholder="Time"
+          name="time"
+          value={ this.state.time }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="text"
+          placeholder="Number of guests"
+          name="number"
+          value={ this.state.number }
+          onChange={ this.handleChange }
+        />
+        <button>Make Reservation</button>
+      </form>
+    )
+  }
+}
+
+export default Form
