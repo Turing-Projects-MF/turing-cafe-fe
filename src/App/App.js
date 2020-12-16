16406ea7f968925d8918b7244ff2aa5791ef7813
mdflynn <mdflynn34@outlook.com>
2020-12-16 09:21:08 -0700

    Retrieve fetched data and set state

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(response => response.json())
+      .then(data => this.setState({ reservations: data }))
+      .catch(error => console.log(error))
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -10,7 +25,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+
         </div>
       </div>
     )
